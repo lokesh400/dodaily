4b36402296df6454f56eabecd23afdbe593a1544
Lokesh <lokeshbadgujjar400@gmail.com>
2026-03-30 01:29:40 +0530

    asdf

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,6 @@
 import { Platform } from 'react-native';
 
-const API_BASE_URL =
-  Platform.OS === 'android' ? 'http://10.60.96.198:4000' : 'http://localhost:4000';
+const API_BASE_URL = "https://dodaily.onrender.com"
 
 function createApiError(message, details = {}) {
   const error = new Error(message);
